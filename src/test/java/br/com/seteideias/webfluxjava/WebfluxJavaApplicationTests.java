81cf1d334f77fb837591ac54551a925e220ee9a1
Cartaxo <carlos.pijanowski@gmail.com>
2023-11-29 20:25:41 -0300

    Initial commit

---
+++ src/test/java/br/com/seteideias/webfluxjava/WebfluxJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.seteideias.webfluxjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
